baa4a79cc52e8b603ec497271e07f5117cd762cb
klevismiho <klevisi@gmail.com>
2016-04-07 11:49:27 +0200

    First commit

---
+++ imports/ui/body.js
@@ -0,0 +1,32 @@
+import { Template } from 'meteor/templating';
+ 
+import { Tasks } from '../api/tasks.js';
+ 
+import './body.html';
+ 
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
+  },
+});
+
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+ 
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+ 
+    // Clear form
+    target.text.value = '';
+  },
+});
